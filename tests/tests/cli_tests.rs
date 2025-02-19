486cc9942ea615b28e81604e9efa806b68ec51e6
Steve Klabnik <steve@steveklabnik.com>
2025-02-19 15:16:56 -0600

    set seed always

--- tests/tests/cli_tests.rs
+++ tests/tests/cli_tests.rs
@@ -6,7 +6,11 @@ fn cli_tests() {
         .case("tests/cmd/**/*.trycmd")
         .env("JJ_TIMESTAMP", "2025-02-05T22:43:34+00:00")
         .env("JJ_OP_TIMESTAMP", "2025-02-05T22:43:34+00:00")
+        .env("JJ_OP_HOSTNAME", "host.example.com")
+        .env("JJ_OP_USERNAME", "steveklabnik")
         .env("TZ", "UTC")
+        .env("LANG", "C.UTF-8")
+        .env("JJ_TZ_OFFSET_MINS", "660")
         .register_bin("tree", Path::new("/usr/bin/tree"))
         .register_bin("cat", Path::new("/usr/bin/cat"))
         .register_bin("sed", Path::new("/usr/bin/sed"));
